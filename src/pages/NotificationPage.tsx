1e23736205a9e72b277b806882d1ea3f127ce95a
CODINGANGRYBIRD <namgung3951@gmail.com>
2023-07-21 15:44:32 +0900

    feat: 알림톡리스트페이지 구조 제작(#5)

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -1,5 +1,67 @@
+import NotificationListBox from "../components/NotificationListBox";
+
 const NotificationPage = () => {
-  return null;
+
+  const alarmData = {
+    "meta": {
+      "totalCount": 0,
+      "size": 0,
+      "count": 0,
+      "page": 0,
+      "hasMore": true
+    },
+    "datas": [
+      {
+        "id": 0,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-20T09:03:50.454Z"
+      },
+      {
+        "id": 1,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-18T09:03:50.454Z"
+      },
+      {
+        "id": 2,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-15T09:03:50.454Z"
+      },
+      {
+        "id": 3,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-14T09:03:50.454Z"
+      },
+      {
+        "id": 4,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-11T09:03:50.454Z"
+      },
+      {
+        "id": 5,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-10T09:03:50.454Z"
+      },
+      {
+        "id": 6,
+        "uuid": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "createDate": "2023-07-07T09:03:50.454Z"
+      }
+    ],
+    "message": "string"
+  }
+
+  return (
+    <div>
+      <p>알림 메시지 리스트</p>
+      {alarmData.datas.map((data) => (
+        <NotificationListBox
+          key={data.id}
+          createDate={data.createDate}
+          id={data.id}
+        />
+      ))}
+    </div>
+  )
 };
 
 export default NotificationPage;
