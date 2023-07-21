3d0e5e57de881a0167fd01924ca3f9dda0e15c17
kangkyeongseo <kks_big@naver.com>
2023-07-21 13:42:38 +0900

    design: Tailwind CSS 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import AppRouter from "./router/Router";
 
 function App() {
   return (
-    <>
+    <div className="w-[390px] h-[844px] px-4 bg-white mx-auto mt-10">
       <AppRouter />
-    </>
+    </div>
   );
 }
 
