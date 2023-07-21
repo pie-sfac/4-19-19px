35795c0cfe63fc2cf5d369aaf23518566f367077
CODINGANGRYBIRD <namgung3951@gmail.com>
2023-07-21 15:27:53 +0900

    feat: 로그인페이지 구조제작(#1)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,15 @@
-const LoginPage = () => {
-  return null;
+import React from "react";
+import Form from "../components/Form";
+import SignupButton from "../components/SignupButton";
+
+const LoginPage: React.FC = () => {
+  return (
+    <>
+      <h1>PoinT</h1>
+      <Form />
+      <SignupButton />
+    </>
+  )
 };
 
 export default LoginPage;
