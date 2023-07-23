64e616b0bff5bfffcb75d536d4c252eca97dbeb8
Hyein Jung <penguin636@naver.com>
2023-07-23 20:34:16 +0900

    design: 퍼스널 레포트 리스트 페이지 스타일 적용 (#20)

--- src/components/PersonalListBox.tsx
+++ src/components/PersonalListBox.tsx
@@ -8,15 +8,15 @@ interface Prop {
 }
 
 const PersonalListBox = ({ uuid, createDate, condition }: Prop) => {
-  const [newDate, setNewDate] = useState("");
+  const [newDate, setNewDate] = useState<string>("");
   const getCreateDate = () => {
     const year = createDate.slice(0, 4);
     const month = createDate.slice(5, 7).padStart(2, "0");
     const day = createDate.slice(8, 10).padStart(2, "0");
-    const date = new Date(year, month - 1, day);
+    const date = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
     const days = ["일", "월", "화", "수", "목", "금", "토"];
     const dayOfWeek = days[date.getDay()];
-    const dateString = `${year}.${month}.${day}.${dayOfWeek}`;
+    const dateString = `${year}. ${month}. ${day}. ${dayOfWeek}`;
     setNewDate(dateString);
   };
   useEffect(() => {
@@ -24,9 +24,9 @@ const PersonalListBox = ({ uuid, createDate, condition }: Prop) => {
   }, []);
   return (
     <Link to={`/personal/${uuid}`}>
-      <div>
-        <h1>{newDate}</h1>
-        <span>{condition}</span>
+      <div className="border-2 rounded-2xl px-8 py-5 mt-4 flex justfiy-center shadow-md">
+        <h1 className="text-2xl font-bold text-[#2D62EA]">{newDate}</h1>
+        <span className="ml-10">{condition}</span>
       </div>
     </Link>
   );
