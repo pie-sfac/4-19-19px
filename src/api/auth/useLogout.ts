2eb553fb3a5e46748b06c097f7366667ee3a7071
eastvvv <pdj93@naver.com>
2023-07-25 16:38:36 +0900

    refactor: useLogout 더미 데이터 추가 (#6)

--- src/api/auth/useLogout.ts
+++ src/api/auth/useLogout.ts
@@ -2,12 +2,12 @@ interface LogoutData {
   message: string;
 }
 
-const useLogout = async () => {
+const useLogout = async (token: string) => {
   const response = await fetch("http://223.130.161.221/mapi/v1/logout", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer { token }",
+      Authorization: `Bearer ${token}`,
     },
   });
   const json = await response.json();
