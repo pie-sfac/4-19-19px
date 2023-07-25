39b0898ad2d51beee0ba678961f55990b3729704
eastvvv <pdj93@naver.com>
2023-07-25 11:10:17 +0900

    add: useLogout 컴포넌트 추가 (#6)

---
+++ src/api/auth/useLogout.ts
@@ -0,0 +1,17 @@
+interface LogoutData {
+  message: string;
+}
+
+const useLogout = async () => {
+  const response = await fetch("http://223.130.161.221/mapi/v1/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer { token }",
+    },
+  });
+  const json = await response.json();
+  return { json };
+};
+
+export default useLogout;
