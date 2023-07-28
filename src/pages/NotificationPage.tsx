ad10d9b2fd12ca96ae27d10926b7a96996a87dd3
CODINGANGRYBIRD <namgung3951@gmail.com>
2023-07-28 14:54:31 +0900

    remove: NotificationPage Header 부분 코드 삭제(#39)

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -1,5 +1,4 @@
 import NotificationListBox from "../components/NotificationListBox";
-import { useNavigate } from "react-router-dom";
 
 const NotificationPage = () => {
   const alarmData = {
@@ -50,43 +49,16 @@ const NotificationPage = () => {
     message: "string",
   };
 
-  const navigate = useNavigate();
-
-  const handleGoBack = () => {
-    if (window.history.length > 2) {
-      navigate(-1);
-    } else {
-      navigate("/");
-    }
-  };
-
   return (
-    <div>
-      <p className="border-b border-b-1 border-gray-300 mx-0 h-14 flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          onClick={handleGoBack}
-        >
-          <path
-            d="M11.6732 2.58002L10.4866 1.40002L3.89322 8.00002L10.4932 14.6L11.6732 13.42L6.25322 8.00002L11.6732 2.58002Z"
-            fill="#505050"
-          />
-        </svg>
-      </p>
-      <div className="flex flex-col">
-        <p className="font-bold mt-5">알림 메시지 리스트</p>
-        {alarmData.datas.map((data) => (
-          <NotificationListBox
-            key={data.id}
-            createDate={data.createDate}
-            id={data.id}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col">
+      <p className="font-bold mt-5">알림 메시지 리스트</p>
+      {alarmData.datas.map((data) => (
+        <NotificationListBox
+          key={data.id}
+          createDate={data.createDate}
+          id={data.id}
+        />
+      ))}
     </div>
   );
 };
