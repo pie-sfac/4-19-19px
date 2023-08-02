32e825c61ba07fcf3552fa5bb0e6a1e43a986272
kangkyeongseo <kks_big@naver.com>
2023-08-02 15:00:41 +0900

    fix: 토큰 유효성 로직 수직

--- src/api/personal/usePersonalReportList.ts
+++ src/api/personal/usePersonalReportList.ts
@@ -1,13 +1,14 @@
 import useSWR from "swr";
 import useTokenCheck from "../../libs/useTokenCheck";
-interface PersonalData {
+import { useEffect, useState } from "react";
+interface PersonalReport {
   id: 0;
   uuid: string;
   createDate: string;
 }
 
 interface PersonReportList {
-  datas: PersonalData[];
+  datas: PersonalReport[];
   meta: {
     totalCount: number;
     size: number;
@@ -15,29 +16,41 @@ interface PersonReportList {
     page: number;
     hasMore: boolean;
   };
-  message: string;
 }
 
-const ACCESS_TOKEN = "accessToken";
-const accessToken = localStorage.getItem(ACCESS_TOKEN);
-const token = accessToken ? JSON.parse(accessToken).token : null;
-
-const fetcher = (url: string) => {
-  return fetch(url, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((res) => res.json());
-};
-
 const usePersonalReportList = () => {
+  const ACCESS_TOKEN = "accessToken";
+  const localAccessToken = localStorage.getItem(ACCESS_TOKEN);
+  const parseAccesstoken = localAccessToken
+    ? JSON.parse(localAccessToken).token
+    : null;
+
+  const [token, setToken] = useState(parseAccesstoken);
+  // 만료된 토큰인지 확인합니다.
   const { isCheckLoading } = useTokenCheck();
+
+  // 토큰 체크가 끝나면 토큰을 setState에 저장합니다.
+  useEffect(() => {
+    setToken(JSON.parse(localAccessToken!).token);
+  }, [isCheckLoading]);
+
+  const fetcher = (url: string) => {
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => res.json());
+  };
+
   const { data, error, isLoading } = useSWR<PersonReportList>(
-    !isCheckLoading && "http://223.130.161.221/mapi/v1/personal-reports",
+    !isCheckLoading &&
+      token &&
+      "http://223.130.161.221/mapi/v1/personal-reports",
     fetcher
   );
+
   return { data, error, isLoading };
 };
 export default usePersonalReportList;
