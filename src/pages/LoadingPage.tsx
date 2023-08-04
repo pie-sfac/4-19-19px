eaa8416aebd39f483e212bf98684c34153e15dc3
CODINGANGRYBIRD <namgung3951@gmail.com>
2023-08-04 15:27:39 +0900

    add: LoadingPage 추가(#79)

---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white">
+      <div className="w-20 h-20 border-8 border-dashed border-blue-500 rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default LoadingPage;
