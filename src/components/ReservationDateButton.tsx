e8582ebc79faaf8ba44b98de7fe3f647d88b9552
Hyein Jung <penguin636@naver.com>
2023-08-08 14:32:26 +0900

    feat: 예약페이지 캘린더 디자인 오류 수정 (#95)

--- src/components/ReservationDateButton.tsx
+++ src/components/ReservationDateButton.tsx
@@ -23,6 +23,8 @@ const SetDateButton = forwardRef<HTMLButtonElement, SetDateButtonProps>(
 
 const ReservationDateButton = () => {
   const { data, mutate } = useSelectedDate();
+  const [activeYear, setActiveYear] = useState(new Date().getFullYear());
+  const [activeMonth, setActiveMonth] = useState(new Date().getMonth());
 
   const handleDateChange = (date: Date) => {
     mutate(date);
@@ -58,6 +60,26 @@ const ReservationDateButton = () => {
     "12월",
   ];
 
+  const handlePrevMonthClick = () => {
+    const newActiveMonth = activeMonth - 1;
+    if (newActiveMonth < 0) {
+      setActiveYear(activeYear - 1);
+      setActiveMonth(11); // 11은 12월을 나타냄
+    } else {
+      setActiveMonth(newActiveMonth);
+    }
+  };
+
+  const handleNextMonthClick = () => {
+    const newActiveMonth = activeMonth + 1;
+    if (newActiveMonth > 11) {
+      setActiveYear(activeYear + 1);
+      setActiveMonth(0); // 0은 1월을 나타냄
+    } else {
+      setActiveMonth(newActiveMonth);
+    }
+  };
+
   return (
     <>
       <div className="text-center">
@@ -69,6 +91,26 @@ const ReservationDateButton = () => {
           popperPlacement="auto"
           customInput={<SetDateButton buttonLabel={buttonLabel} />}
           calendarClassName="bg-white border-[#2D62EA] border-2 rounded-lg"
+          filterDate={(date) => {
+            const currentDate = new Date();
+            const currentYear = currentDate.getFullYear();
+            const currentMonth = currentDate.getMonth();
+
+            const selectedYear = date.getFullYear();
+            const selectedMonth = date.getMonth();
+
+            return (
+              (selectedYear === currentYear &&
+                selectedMonth === currentMonth) ||
+              (selectedYear === activeYear && selectedMonth === activeMonth)
+            );
+          }}
+          onMonthChange={(date) => {
+            setActiveMonth(date.getMonth());
+          }}
+          onYearChange={(date) => {
+            setActiveYear(date.getFullYear());
+          }}
           renderCustomHeader={({
             date,
             changeYear,
@@ -78,14 +120,40 @@ const ReservationDateButton = () => {
             prevMonthButtonDisabled,
             nextMonthButtonDisabled,
           }) => (
-            <div className="flex justify-center bg-[#2D62EA] mb rounded-none">
+            <div className="flex justify-center bg-[#2D62EA] mb-1 rounded-none">
               <span
-                onClick={decreaseMonth}
+                onClick={() => {
+                  if (activeMonth === 0 && activeYear === 2000) {
+                    null;
+                  } else {
+                    decreaseMonth();
+                    handlePrevMonthClick();
+                  }
+                }}
                 className={`mr-4 text-base font-light text-white ${
                   prevMonthButtonDisabled ? "pointer-events-none" : ""
                 }`}
               >
-                {"<"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="19"
+                  viewBox="0 0 30 48"
+                  fill={
+                    activeMonth === 0 && activeYear === 2000
+                      ? "#2D62EA"
+                      : "white"
+                  }
+                >
+                  <path
+                    d="M29.64 42.36L11.32 24L29.64 5.64L24 0L0 24L24 48L29.64 42.36Z"
+                    fill={
+                      activeMonth === 0 && activeYear === 2000
+                        ? "#2D62EA"
+                        : "white"
+                    }
+                  />
+                </svg>
               </span>
               <select
                 value={date.getFullYear()}
@@ -114,12 +182,43 @@ const ReservationDateButton = () => {
               </select>
 
               <span
-                onClick={increaseMonth}
+                onClick={() => {
+                  if (
+                    activeMonth === 11 &&
+                    activeYear === new Date().getFullYear()
+                  ) {
+                    null;
+                  } else {
+                    increaseMonth();
+                    handleNextMonthClick();
+                  }
+                }}
                 className={`ml-4 text-base font-light text-white ${
                   nextMonthButtonDisabled ? "pointer-events-none" : ""
                 }`}
               >
-                {">"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="19"
+                  viewBox="0 0 30 48"
+                  fill={
+                    activeMonth === 11 &&
+                    activeYear === new Date().getFullYear()
+                      ? "#2D62EA"
+                      : "white"
+                  }
+                >
+                  <path
+                    d="M0 42.36L18.32 24L0 5.64L5.64 0L29.64 24L5.64 48L0 42.36Z"
+                    fill={
+                      activeMonth === 11 &&
+                      activeYear === new Date().getFullYear()
+                        ? "#2D62EA"
+                        : "white"
+                    }
+                  />
+                </svg>
               </span>
             </div>
           )}
